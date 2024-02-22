08e33cebb4522db0629b3a7e9651ee8bd5721cc2
Sam Holden <sam.holden@capgemini.com>
2024-02-22 18:08:09 +0000

    app src dir

---
+++ src/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Your Name"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """Main entry point of the app"""
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
