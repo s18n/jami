722a2216192576a7e58131b6bfa3039210455f44
Sam Holden <sam.holden@digital.cabinet-office.gov.uk>
2024-03-04 16:02:35 +0000

    refactor(fmt): formatting files

--- src/common/policy.py
+++ src/common/policy.py
@@ -8,10 +8,14 @@ class Policy:  # pylint: disable=too-few-public-methods
     """policy base class"""
 
     def __init__(self, data):
+        self.name = data["name"]
         self.id = data["id"]
-        self.policy_type = data["policy_type"]
         self.description = data["description"]
+
         self.accounts = data["accounts"]
+        self.ous = data["ous"]
+
+        self.policy_type = data["policy_type"]
         self.ticket_number = data["ticket_number"]
 
 
@@ -25,6 +29,8 @@ class Eligibility(Policy):  # pylint: disable=too-few-public-methods
     def __init__(self, data):
         super().__init__(data)
         self.permissions = data["permissions"]
+        self.duration = data["duration"]
+        self.approval_required = data["approval_required"]
 
 
 class Approval(Policy):  # pylint: disable=too-few-public-methods
@@ -37,3 +43,4 @@ class Approval(Policy):  # pylint: disable=too-few-public-methods
     def __init__(self, data):
         super().__init__(data)
         self.type = data["type"]
+        self.group_ids = data["group_ids"]
