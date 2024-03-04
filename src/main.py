556b16d5b813ddc47b62ee2e0fe98fb528a27146
Sam Holden <sam.holden@digital.cabinet-office.gov.uk>
2024-03-04 15:45:31 +0000

    feat(policy): simplify policy classes, support for doc separators

--- src/main.py
+++ src/main.py
@@ -4,42 +4,84 @@ Read yaml, produce object
 """
 
 
-from os import getcwd, listdir
-from os.path import isfile, join
+from os import getcwd, listdir, path
+from pprint import pprint
 
+import yaml
 from logzero import logger
 
-from common.policy import Policy  # pylint: disable=import-error
+from common.policy import Approval, Eligibility  # pylint: disable=import-error
+
+
+def load_policies_from_yaml(policy_path):
+    """function to load policy files from local policy directory & initialise
+    as class objects.
+
+    Args:
+        policy_path (str): absolute path to policy directory.
+
+    Returns:
+        list: policy object list.
+    """
+    # initialise empty list to populate with policy objects
+    policies = []
+
+    # list all files in policy folder
+    for policy_file in listdir(policy_path):
+
+        # only load ".yaml" files
+        if policy_file.endswith(".yaml"):
+
+            # open policy file
+            try:
+                with open(
+                    path.join(policy_path, policy_file), "r", encoding="UTF-8"
+                ) as file:
+
+                    # load separate yaml documents
+                    for policy_doc in yaml.safe_load_all(file):
+
+                        # initialise object class based on policy type
+                        match policy_doc["policy_type"]:
+                            case "eligibility":
+                                policies.append(Eligibility(policy_doc))
+                            case "approval":
+                                policies.append(Approval(policy_doc))
+                            case _:
+                                # fmt: off
+                                logger.error(
+                                    "type unkown: %s",
+                                    policy_doc["policy_type"]
+                                )
+                                # fmt: on
+            except FileNotFoundError as error:
+                # fmt: off
+                logger.error(
+                    "failed to load policy: %s\n%s",
+                    policy_file,
+                    error
+                )
+                # fmt: on
+
+    # return list of policy objects
+    return policies
 
 
 def main():
     """Main entry point of the app"""
+    policy_data = []
 
-    # initialise policy lists
-    elig_data = []
-    appr_data = []
+    # construct path to policy directory
+    policy_path = path.join(getcwd(), "policies")
+    logger.info("policy path: %s", policy_path)
 
-    # fetch policy files
-    path = getcwd()
-    elig_path = f"{path}/policies/eligibility/"
-    appr_path = f"{path}/policies/approval/"
-    elig_files = [f for f in listdir(elig_path) if isfile(join(elig_path, f))]
-    appr_files = [f for f in listdir(appr_path) if isfile(join(appr_path, f))]
+    # load & initialise individual policy documents
+    policy_data = load_policies_from_yaml(policy_path)
 
-    # collect approval policy filenames
-    for file in appr_files:
-        logger.info("fetch appr policy...")
-        appr_data.append(Policy(f"{appr_path}{file}"))
+    # print all policies
+    pprint([(policy.id, policy.description) for policy in policy_data])
 
-    # print all appr policies
-    print([policy.policy for policy in appr_data])
-
-    # collect elgibility policy filenames
-    for file in elig_files:
-        elig_data.append(Policy(f"{elig_path}{file}"))
-
-    # print all elig policies
-    print([policy.policy for policy in elig_data])
+    # [TODO: execute terraform classes]
 
 
 if __name__ == "__main__":
